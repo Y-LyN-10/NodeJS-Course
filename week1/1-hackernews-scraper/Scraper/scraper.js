bfabc221b04e08a18c2d01ae4d15032fabb08f30
Yulia Tenincheva <yulia.ten@dezigner.ru>
2014-10-18 23:06:58 +0300

    Save articles and comments in thefile system - but only the last one

--- week1/1-hackernews-scraper/Scraper/scraper.js
+++ week1/1-hackernews-scraper/Scraper/scraper.js
@@ -1,20 +1,100 @@
 'use strict';
 
-var express = require ('express'),
+var express = require('express'),
     bodyParser = require('body-parser'),
     request = require('request'),
+    articles = require('./articles.json'),
+    fs = require('fs'),
+    os = require('os'),
+//    Q = require('q'),
+//    deferred = Q.defer(),
     app = express();
+
+var maxItemUrl = 'https://hacker-news.firebaseio.com/v0/maxitem.json',
+    itemsUrl = 'https://hacker-news.firebaseio.com/v0/item/';
+
+var maxItem;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-setInterval(function() {
-    /* TODO: 1. Check for new articles on hacker-news:
-            - Get maxItem (id)
-            - Get all articles by ID in the range from lastKnownArticleID to maxItem
-       TODO: 2. Save all new articles in the file system */
-}, 60 * 3000);
+function getSavedArticlesMaxItem() {
+    fs.readFile('articles.json', 'utf8', function (err, data) {
+        if (err) {
+            console.log(err);
+        }
+        var objData = JSON.parse(data);
+        var idArray = [];
 
-request.get('https://hacker-news.firebaseio.com/v0/maxitem.json', function(req))
+        for (var property in objData) {
+            if (objData.hasOwnProperty(property)) {
+                idArray.push(property);
+            }
+        }
+
+        maxItem = idArray[idArray.length-1];
+    });
+}
+
+getSavedArticlesMaxItem();
+
+setInterval(function () {
+    // Check for new items on hacker-news:
+    getMaxItem();
+}, 60 * 100);
+
+function getMaxItem() {
+    request.get(maxItemUrl, function (err, res, body) {
+        if (!err) {
+            var resultsObj = JSON.parse(body);
+            if(resultsObj > maxItem){
+                for (var i = maxItem+1; i <= resultsObj; i+=1) {
+                    getArticle(i);
+                    console.log('New item with ID: ' + i);
+                }
+                maxItem = resultsObj;
+            }
+        }
+    });
+}
+
+function getArticle(itemID) {
+    request.get((itemsUrl + itemID + '.json'), function (err, res, item) {
+        if (err) {
+            console.log(err);
+        }
+
+        item = JSON.parse(item);
+        var objItem = {};
+        objItem[itemID] = item;
+
+        saveItemToFS(itemID, objItem);
+    });
+}
+
+function saveItemToFS(id, item) {
+    var saveData = JSON.stringify(item, os.EOL, ' ');
+    if(item[id].type === 'story'){
+        fs.writeFile('./articles.json', saveData, 'utf8', function (err) {
+            if (err) {
+                throw err;
+            }
+        });
+        console.log('saved article');
+    } else if(item[id].type === 'comment'){
+        fs.writeFile('./comments.json', saveData, 'utf8', function (err) {
+            if (err) {
+                throw err;
+            }
+        });
+        console.log('saved comment');
+    } else {
+        console.log('unrecognized item');
+        console.log(item);
+    }
+}
+
+// TODO: 2. Save all new articles in the file system
+// - Get all articles by ID in the range from lastMaxItem to maxItem
 
 app.listen(3001);
