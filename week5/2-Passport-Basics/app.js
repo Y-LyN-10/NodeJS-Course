223aa50a5b1f891a9184c399a661986b3815027c
Yulia Tenincheva <yulia.ten@dezigner.ru>
2014-12-02 12:09:35 +0200

    Start from scratch. Solve step-by-step

--- week5/2-Passport-Basics/app.js
+++ week5/2-Passport-Basics/app.js
@@ -1,56 +1,35 @@
-var express = require("express"),
-	config = require('./config.json'),
-    path = require('path'),
-    querystring = require('querystring'),
-    tolmort = process.argv[2],
-    data = require('./data/' + tolmort),
-    MongoClient = require('mongodb').MongoClient,
-    url = 'mongodb://localhost:27017/users',
-    util = require('util'),
+var express = require('express'),
+    app = express(),
     bodyParser = require('body-parser'),
-	// passport = require('passport'),
-	// LocalStrategy = require('passport-local').Strategy,
-    user = {},
-    app = express();
+    port = parseInt(process.env.PORT, 10) || 3000;
 
-
-app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+// TODO: Connect with MongoDb
+
+// TODO: Use passport to implement the login / logout functionallity
+
+var user = {};
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
 
 app.post('/login', function(req, res) {
-	var chunk = '';
+    console.log(req.body);
 
-    req.on('data', function (data, err) {
-    	if(err){
-			console.log(err);
-		}
-        
-        chunk += data;
-    });
+	user['username'] = req.body.username;
+	user['password'] = req.body.password;
 
-    req.on('end', function () {
-    	chunk = util.inspect(querystring.parse(chunk));
-		res.end(chunk);
-
-		// user['username'] = chunk;
-		// user['password'] = chunk['password'];
-    });
+    // TODO: checks for existing user, etc...
+    res.send(user);
 });
 
-MongoClient.connect(url, function(err, db) {
-    var collectionName = path.basename(tolmort, '.json'),
-        collection = db.collection(collectionName);
-
-    console.log('Connected correctly to the server');
-
-    // collection.insert(data, function(err, result) {
-    //     console.log("Inserted something into the document collection");
-    //     console.log(result);
-    // });
-
-	// collecion.find();
-
-    db.close();
+app.post('/logout', function(req, res) {
+    // TODO: ...
 });
 
-app.listen(3000);
+console.log("Server running on port: " + port);
+app.listen(port);
