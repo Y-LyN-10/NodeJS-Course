9332aa01cf6655c5413b697bf87c327a927b8a5c
Yulia Ten <Y-LyN-10@users.noreply.github.com>
2014-12-11 20:17:06 +0200

    Edit code formatting in GitHub

--- exam1/1-Smart-Phonebook/app.js
+++ exam1/1-Smart-Phonebook/app.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-	app = express(),
-	port = process.env.PORT || 3000;
+    app = express(),
+    port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
